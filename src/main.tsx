3eebdad263284e13b9d9dddb2f515df2947cfed3
enikitas <enikitas@netcomapny.com>
2025-02-10 13:07:55 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -23,6 +23,7 @@ async function enableMocking() {
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
   return worker.start();
+  //
 }
 
 // Start mocking if in development mode
